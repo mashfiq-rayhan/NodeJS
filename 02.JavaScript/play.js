b61e92135e70d8a94bfd88de95a3ec5bae834efe
mashfiq-rayhan <mashfiq.rayhan.ovi@gmail.com>
2024-09-03 00:21:21 +0600

    Day 1 Module 1 2 3

---
+++ 02.JavaScript/play.js
@@ -0,0 +1,107 @@
+// const name = "Mash";
+// const age = 30;
+// const hasHobbies = true;
+
+// function summerizeUser(userName, userAge, userhasHobby) {
+//   return (
+//     "Name is " +
+//     userName +
+//     ", age is " +
+//     userAge +
+//     " and the user has hobbies: " +
+//     userhasHobby
+//   );
+// }
+
+// const summerizeUser = (userName, userAge, userhasHobby) => {
+//   return (
+//     "Name is " +
+//     userName +
+//     ", age is " +
+//     userAge +
+//     " and the user has hobbies: " +
+//     userhasHobby
+//   );
+// };
+
+// console.log(summerizeUser(name, age, hasHobbies));
+
+// const add = (a,b) => a+b;
+// console.log(add(1, 2));
+
+// const addOne = (a) => a + 1;
+// console.log(addOne(10));
+
+// const addRandom = () => 1+2;
+// console.log(addRandom());
+
+// const person = {
+//     name: 'Mash',
+//     age: 30,
+//     greet() {
+//         console.log('Hi, I am ' + this.name)
+//     }
+// };
+
+// const person = {
+//     name: 'Mash',
+//     age: 30,
+//     greet: function() {
+//         console.log('Hi, I am ' + this.name)
+//     }
+// };
+
+// person.greet()
+// console.log(person);
+
+// const copiedPerson = {...person};
+// console.log(copiedPerson);
+
+// const hobbies = ["Sports", "Cooking"];
+
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+// hobbies.push('Programming');
+// console.log(hobbies);
+
+// const copiedArr = hobbies.slice();
+// const copiedArr = [...hobbies];
+// console.log(copiedArr);
+
+// const toArray = (...args) => {
+//     return args;
+// };
+
+// console.log(toArray(1,2,3,4));
+
+// const person = {
+//     name: 'Mash',
+//     age: 30,
+//     greet() {
+//         console.log('Hi, I am ' + this.name)
+//     }
+// };
+
+// person.greet();
+
+// const { name, age } = person;
+// console.log(name, age);
+
+// const printName = ({ name, age, greet }) => {
+//     console.log(name);
+//     console.log(age);
+//     greet();
+// };
+
+// printName(person);
+
+// const hobbies = ["Sports", "Cooking"];
+
+// const [ hobby1, hobby2 ] = hobbies;
+// console.log(hobby1, hobby2);
+
