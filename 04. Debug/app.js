acb955d5ce8cce280060d24ea5688ab7452d687c
mashfiq-rayhan <mashfiq.rayhan.ovi@gmail.com>
2024-09-03 12:52:55 +0600

    Day 2 Module 4

---
+++ 04. Debug/app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
+
+// 160 0 180
+// 191 191 191
