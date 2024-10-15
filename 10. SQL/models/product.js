4b5a34e1461c9daef95ec8990d61d062615efa67
mashfiq_rayhan <mashfiq.rayhan.ovi@gmail.com>
2024-10-15 20:15:16 +0600

    Day 3 Module 10

---
+++ 10. SQL/models/product.js
@@ -0,0 +1,27 @@
+const db = require("../util/database");
+
+const Cart = require("./cart");
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, price, description) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+  }
+
+  save() {
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', [this.title, this.price, this.imageUrl, this.description]);
+  }
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
+  }
+
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+  }
+
+  static deleteById(id) {}
+};
